f2a27398a1316980758ad3c3d2888973b94da57a
Jak Spalding <jak.spalding@gmail.com>
2012-09-17 15:36:51 +0100

    initial import of responsive images component

---
+++ scripts/components/responsiveimages/responsiveimagesMediator.js
@@ -0,0 +1,38 @@
+define([
+    'require',
+    'jquery',
+    'signals',
+    'json!./config.json',
+    'superclasses/mediator',
+    './modules/imageryModule'
+], function (require, $, Signal, config, Mediator, Imagery) {
+    var mediator = Mediator.extend({
+
+        config: config,
+
+        init: function (settings) {
+            var that = this, key;
+            this.addModules(Imagery);
+            this._super(settings);
+        },
+
+        run: function () {
+            this.modules.Imagery.evaluate();
+        },
+
+        /**
+         * Pass up the signal to the page mediator, so it can do what it likes
+         * @return {void}
+         */
+        onImageryEvaluated: function () {
+            this.signals.Ran.dispatch();
+        },
+
+        signals: {
+            // Fires every time new images are loaded
+            Ran: new Signal()
+        }
+    });
+
+    return mediator;
+});
