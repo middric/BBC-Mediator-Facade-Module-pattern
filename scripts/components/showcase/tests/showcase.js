ae881210d75e6b3cc9e9e061cf562c28edda8c13
Rich Middleditch <richmiddleditch@gmail.com>
2012-09-18 16:08:22 +0100

    Additional showcase tests

--- scripts/components/showcase/tests/showcase.js
+++ scripts/components/showcase/tests/showcase.js
@@ -1,8 +1,31 @@
 define(['showcaseMediator'], function (Showcase) {
     describe("Showcase", function () {
-        var sc = new Showcase();
+        var sc = new Showcase(),
+            loaded = false;
+        
+        sc.signals.Loaded.addOnce(function () { loaded = true; });
+
         it("has instantiated attached modules", function () {
-            expect(sc.moduleConstructors.length).toEqual(Object.keys(sc.modules).length);
+            var size = 0, key;
+            for (key in sc.modules) {
+                if (sc.modules.hasOwnProperty(key)) {
+                    size++;
+                }
+            }
+            expect(sc.moduleConstructors.length).toEqual(size);
+        });
+
+        it("has fired the Loaded signal", function () {
+            waitsFor(
+                function () {
+                    return loaded;
+                },
+                "Showcase never completed loading or never fired the Loaded signal",
+                10000
+            );
+            runs(function () {
+                expect(loaded).toEqual(true);
+            });
         });
     });
 });
